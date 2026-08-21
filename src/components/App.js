e0cf76c808cf3e10d4513c1aba8216b28024a2a6
Supra-Mk4-1998 <sr7773879@gmail.com>
2026-08-21 15:28:47 +0530

    push 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
+import React, { useState } from "react";
+import Todo from "./Todo";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
 
-const App = () => {
+  const addTodo = () => {
+    if (input.trim() === "") {
+      return;
+    }
+
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Todo List</h1>
+
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter a todo"
+      />
+
+      <button onClick={addTodo}>
+        Add Todo
+      </button>
+
+      <Todo
+        todos={todos}
+        deleteTodo={deleteTodo}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Todo({ todos, deleteTodo }) {
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <li key={index}>
+          {todo}
+
+          <button onClick={() => deleteTodo(index)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Todo;
